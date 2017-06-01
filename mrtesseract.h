e716f70e1101e737a3191a3db141ae81573d2d4d
imsityrain <1293830063@qq.com>
2017-06-01 23:24:42 +0800

    fix bugs on tesseract

--- mrtesseract.h
+++ mrtesseract.h
@@ -19,11 +19,83 @@
 #ifdef USE_ENG_DATA
 	const char* langname = "eng";
 #else
-#define TESSERACT_PASSPORT 1
-#if TESSERACT_PASSPORT
-	const char* langname = "passport";
+	#define TESSERACT_PASSPORT 1
+	#if TESSERACT_PASSPORT
+		const char* langname = "chi_sim";
+	#endif
 #endif
+
+class MRTessMNumRecognier
+{
+public:
+	static MRTessMNumRecognier *getInstance()
+	{
+		static MRTessMNumRecognier instance;
+		return &instance;
+	}
+#if TESS_WITH_OPENCV
+	std::string recog(const cv::Mat &img)
+	{
+		if (!img.data)
+		{
+			std::cout << "cannot load image" << std::endl;
+			return "";
+		}
+		api.SetImage((uchar*)img.data, img.cols, img.rows, img.channels(), img.step);
+		char*ocrresult = api.GetUTF8Text();
+		std::string str = ocrresult;
+		delete[]ocrresult;
+		return str;
+	}
 #endif
+	tesseract::TessBaseAPI  api;
+private:
+	MRTessMNumRecognier(){
+		api.SetPageSegMode(tesseract::PSM_SINGLE_CHAR);
+		api.Init("", "eng");
+		api.SetVariable("tessedit_char_whitelist", "0123456789");
+	}
+	~MRTessMNumRecognier()
+	{
+		api.Clear();
+		api.End();
+	}
+};
+
+class MRTessChineseRecognier{
+public:
+	static MRTessChineseRecognier *getInstance()
+	{
+		static MRTessChineseRecognier instance;
+		return &instance;
+	}
+#if TESS_WITH_OPENCV
+	std::string recog(const cv::Mat &img)
+	{
+		if (!img.data)
+		{
+			std::cout << "cannot load image" << std::endl;
+			return "";
+		}
+		api.SetImage((uchar*)img.data, img.cols, img.rows, img.channels(), img.step);
+		char*ocrresult = api.GetUTF8Text();
+		std::string str = ocrresult;
+		delete[]ocrresult;
+		return str;
+	}
+#endif
+	tesseract::TessBaseAPI  api;
+private:
+	MRTessChineseRecognier(){
+		api.SetPageSegMode(tesseract::PSM_SINGLE_CHAR);
+		api.Init("", "chi_sim");
+	}
+	~MRTessChineseRecognier()
+	{
+		api.Clear();
+		api.End();
+	}
+};
 
 class MRTesseract
 {
